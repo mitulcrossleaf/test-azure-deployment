9dbd7898af7cb7df22e8d1c09b9450e25ec8b078
mitulcrossleaf <mitulkanani@crossleaf.ca>
2025-10-15 16:52:05 +0530

    remove standalone

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+}
 
 export default nextConfig
